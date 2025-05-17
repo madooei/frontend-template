2caa066a2a778d834fcf139162593ca1d80ca434
Ali Madooei <alimadooei@gmail.com>
2025-05-17 17:32:20 -0400

    Refactor NotFoundPage to enhance styling and navigation
    - Integrated the `cn` utility function for dynamic class name management in the NotFoundPage component.
    - Added a DEBUG constant to conditionally apply a background color for debugging purposes.
    - Replaced the standard Link component with the new DecoratedLink component for improved navigation styling.

--- src/pages/not-found-page.tsx
+++ src/pages/not-found-page.tsx
@@ -1,13 +1,21 @@
-import { Link } from "react-router";
+import { cn } from "@/lib/utils";
+import { DecoratedLink } from "@/components/decorated-link";
+
+const DEBUG = false;
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-8">
+    <div
+      className={cn(
+        "flex flex-col items-center justify-center w-full min-h-screen gap-8",
+        {
+          "bg-red-500": DEBUG,
+        },
+      )}
+    >
       <h1 className="text-4xl">404 - Page Not Found</h1>
       <p className="text-lg">The page you are looking for does not exist.</p>
-      <Link to="/" className="hover:underline">
-        Go back to the home page
-      </Link>
+      <DecoratedLink to="/">Go back to the home page</DecoratedLink>
     </div>
   );
 };
