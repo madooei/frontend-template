c83a969c1d42ed6e8af6a8edb188b47c9f11631b
Ali Madooei <alimadooei@gmail.com>
2025-05-26 23:34:29 -0400

    React-router data mode: Hello-world!
    Refactor routing setup to use RouterProvider and remove BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
 import "@/styles/index.css"; // Keep this line at the top so richColors can be used in the Toaster
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import { Toaster } from "@/components/ui/sonner";
 import App from "./App.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
 
 const root = document.getElementById("root");
 
 createRoot(root!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-      <Toaster richColors position="bottom-right" />
-    </StrictMode>
-  </BrowserRouter>,
+  // <BrowserRouter>
+  //   <StrictMode>
+  //     <App />
+  //     <Toaster richColors position="bottom-right" />
+  //   </StrictMode>
+  // </BrowserRouter>,
+  <RouterProvider router={router} />
 );
