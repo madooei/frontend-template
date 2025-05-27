72ccc9529ef4421a2797a91069ccd874b10e0087
Ali Madooei <alimadooei@gmail.com>
2025-05-27 00:07:15 -0400

    Refactor routing to use RouterProvider and createBrowserRouter; update App component structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,14 @@
 import "@/styles/index.css"; // Keep this line at the top so richColors can be used in the Toaster
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
 import { Toaster } from "@/components/ui/sonner";
 import App from "./App.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
 
 const root = document.getElementById("root");
 
 createRoot(root!).render(
-  // <BrowserRouter>
-  //   <StrictMode>
-  //     <App />
-  //     <Toaster richColors position="bottom-right" />
-  //   </StrictMode>
-  // </BrowserRouter>,
-  <RouterProvider router={router} />
+    <StrictMode>
+      <App />
+      <Toaster richColors position="bottom-right" />
+    </StrictMode>
 );
