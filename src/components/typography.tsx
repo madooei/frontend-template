5013c715ba95057527bbd334b25b2ceeb71e8db7
Ali Madooei <alimadooei@gmail.com>
2025-05-29 18:01:38 -0400

    refactor: enhance Typography component to accept additional props for greater flexibility

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -4,8 +4,13 @@ import type { PropsWithChildren } from "react";
 const Typography = ({
   children,
   className,
-}: PropsWithChildren<{ className?: string }>) => {
-  return <div className={cn("typography", className)}>{children}</div>;
+  ...props
+}: PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => {
+  return (
+    <div className={cn("typography", className)} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Typography;
