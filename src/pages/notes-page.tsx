d47b1296f40dac6aa26be6b6612f7ce5ba414e24
Ali Madooei <alimadooei@gmail.com>
2025-06-13 13:50:26 -0400

    feat: create NotesPage component to display a list of notes

---
+++ src/pages/notes-page.tsx
@@ -0,0 +1,20 @@
+import type { NoteType } from "@/types/note-types";
+import { useLoaderData } from "react-router";
+import Typography from "@/components/typography";
+
+const NotesPage: React.FC = () => {
+  const { notes } = useLoaderData<{ notes: NoteType[] }>();
+
+  return (
+    <Typography >
+      <h3>Notes!</h3>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note.text}</li>
+        ))}
+      </ul>
+    </Typography>
+  );
+};
+
+export default NotesPage;
